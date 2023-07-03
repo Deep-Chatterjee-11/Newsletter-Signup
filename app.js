9470778b91e0377d121822b835c26fbf9eb9251f
Deep Jyoti Chatterjee <132761088+Deep-Chatterjee-11@users.noreply.github.com>
2023-07-03 21:00:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            //members is an array of objects
+            //we have only 1 object as we subscribe one member at a time
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/9f765e481d";
+    const options = {
+        method: "POST",
+        auth: "deepjyoti:93b8c8f38cebc6624d5a54c09917faab-us21"
+    };
+
+    const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
+
+
+
+// MailChimp API key
+// 93b8c8f38cebc6624d5a54c09917faab-us21
+// 93b8c8f38cebc6624d5a54c09917faab-us21
+
+
+// List id or Audience id
+// 9f765e481d
+// 9f765e481d
